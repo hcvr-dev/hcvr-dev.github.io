a1447112888edf5c778e268097f18655493ba5ca
hcvr-dev <60954929+hcvr-dev@users.noreply.github.com>
2020-02-12 12:05:26 -0500

    Setup

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20190118_112320",
+      "name": "IMG_20190118_112320",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.8686610682616074,
+        "pitch": 0.00041753965211910327,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49242935753265904,
+          "pitch": 0.04030971669778971,
+          "rotation": 0,
+          "target": "1-img_20190111_124117"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7395615958997332,
+          "pitch": 0.024212210520415667,
+          "title": "Druskin Lounge",
+          "text": "What event is held here every winter?"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20190111_124117",
+      "name": "IMG_20190111_124117",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 2.139252368370375,
+        "pitch": 0.04612399303811543,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20190118_111314",
+      "name": "IMG_20190118_111314",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.8921052778221359,
+        "pitch": 0.02591592833822176,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
